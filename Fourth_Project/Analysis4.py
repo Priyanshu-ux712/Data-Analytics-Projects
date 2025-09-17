a940d91d8e367bbded71a73b11abf68777452aee
Priyanshu <priyanshukumar9053@gmail.com>
2025-09-17 19:50:05 +0530

    Add files via upload

---
+++ Fourth_Project/Analysis4.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("C:/Users/priyanshu/OneDrive/Documents/Data_Analytics_Project_4/Rich_Global_Condom_Usage_Dataset.csv")
+# print(data)
+# print(data.isnull().sum())
+# print(data.describe())
+
